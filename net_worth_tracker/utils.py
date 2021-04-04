5fd4a72115f70c391b57d5d7fd55f9d71f568c9a
Bas Nijholt <bas@nijho.lt>
2021-04-04 13:15:34 +0200

    add read_config to utils

--- net_worth_tracker/utils.py
+++ net_worth_tracker/utils.py
@@ -1,4 +1,9 @@
+from configparser import ConfigParser
+from pathlib import Path
+
 import keyring
+
+DEFAULT_CONFIG = Path("~/.config/crypto_etf.conf").expanduser()
 
 
 def get_password(username: str, service: str):
@@ -6,3 +11,28 @@ def get_password(username: str, service: str):
     if not pw:
         raise Exception(f"python -m keyring set {service} {username}")
     return pw
+
+
+def read_config(path: Path = DEFAULT_CONFIG):
+    """Create a config file at ~/.config/crypto_etf.conf with the
+    following information and structure:
+        [binance]
+        api_key = ...
+        api_secret = ...
+
+        [bsc]
+        address = ...
+
+        [bscscan]
+        api_key = ...
+
+    Uses https://docs.python.org/3/library/configparser.html
+    """
+    config = ConfigParser()
+    config.read(DEFAULT_CONFIG)
+    if config == []:
+        print(
+            "Create a config file at ~/.config/crypto_etf.conf with api keys \
+            according to configparser."
+        )
+    return config
