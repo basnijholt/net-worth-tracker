cc6b3c9d1438ab6987014103c278fa6f03d63c40
Bas Nijholt <bas@nijho.lt>
2021-04-04 13:30:36 +0200

    add several data processing functions to utils

--- net_worth_tracker/utils.py
+++ net_worth_tracker/utils.py
@@ -1,7 +1,11 @@
+import datetime
+import json
+from collections import defaultdict
 from configparser import ConfigParser
 from pathlib import Path
 
 import keyring
+import pandas as pd
 
 DEFAULT_CONFIG = Path("~/.config/crypto_etf.conf").expanduser()
 
@@ -36,3 +40,174 @@ def read_config(path: Path = DEFAULT_CONFIG):
             according to configparser."
         )
     return config
+
+
+def combine_balances(*balances_dicts):
+    balances = defaultdict(float)
+    for bal in balances_dicts:
+        for coin, amount in bal.items():
+            balances[coin] += amount
+    return dict(sorted(balances.items()))
+
+
+def save_data(
+    balances_binance,
+    balances_blockfi,
+    balances_exodus,
+    balances_nexo,
+    balances_trust,
+    balances_bsc,
+    eur_balances,
+    bsc,
+    folder=Path("data"),
+):
+    balances_per_category = dict(
+        binance=balances_binance,
+        blockfi=balances_blockfi,
+        exodus=balances_exodus,
+        nexo=balances_nexo,
+        trust=balances_trust,
+        bsc=balances_bsc,
+    )
+    data = dict(
+        balances_per_category=dict(
+            binance=balances_binance,
+            blockfi=balances_blockfi,
+            exodus=balances_exodus,
+            nexo=balances_nexo,
+            trust=balances_trust,
+            bsc=balances_bsc,
+        ),
+        balances=combine_balances(*balances_per_category.values()),
+        eur_balances=eur_balances,
+        defi=dict(bsc=bsc),
+    )
+
+    dt_str = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    fname = dt_str + ".json"
+    folder.mkdir(exist_ok=True)
+    with open(folder / fname, "w") as f:
+        json.dump(data, f, indent="  ")
+
+
+def load_data(folder=Path("data")):
+    fnames = sorted(Path(folder).glob("*.json"))
+    datas = {}
+    for fname in fnames:
+        with fname.open("r") as f:
+            dt = datetime.datetime.strptime(fname.with_suffix("").name, "%Y%m%d-%H%M%S")
+            datas[dt] = json.load(f)
+    return datas
+
+
+def data_to_df(date, data):
+    lst = [
+        {
+            "date": date,
+            "symbol": coin,
+            "amount": data["balances"][coin],
+            "value": data["eur_balances"].get(coin),
+        }
+        for coin in data["balances"]
+    ]
+    for d in lst:
+        if d["value"] is not None:
+            d["price"] = d["value"] / d["amount"]
+        for where, info in data["balances_per_category"].items():
+            d["ratio_in_" + where] = info.get(d["symbol"], 0) / d["amount"]
+
+    df = pd.DataFrame(lst)
+    for col in df.columns:
+        if col.startswith("ratio_in"):
+            df[col.replace("ratio_in", "value_in")] = df[col] * df["value"]
+    return df
+
+
+def datas_to_df(datas):
+    dfs = [data_to_df(date, data) for date, data in datas.items()]
+    return pd.concat(dfs).sort_values("date")
+
+
+def get_df(key, datas):
+    df = pd.DataFrame(
+        [data[key] for data in datas.values()], [date for date in datas.keys()]
+    ).sort_index()
+    order = df.iloc[-1].sort_values(ascending=False).index
+    return df[order]
+
+
+def get_df_wallet(wallet, datas):
+    df = pd.DataFrame(
+        [data["balances_per_category"][wallet] for data in datas.values()],
+        [date for date in datas.keys()],
+    ).sort_index()
+    order = df.iloc[-1].sort_values(ascending=False).index
+    return df[order]
+
+
+def at_time_ago(df, time_ago):
+    now = datetime.datetime.now()
+    dt = now - time_ago
+    i = (df.date - dt).abs().argmin()
+    date = df.date.iloc[i]
+    return df[df.date == date].set_index("symbol")
+
+
+def overview_df(df):
+    df_24h = at_time_ago(df, datetime.timedelta(hours=24))
+    df_last = at_time_ago(df, datetime.timedelta(0))
+    df_1w = at_time_ago(df, datetime.timedelta(days=7))
+    df_last["1w price (%)"] = 100 * (df_last.price - df_1w.price) / df_1w.price
+    df_last["24h price (%)"] = 100 * (df_last.price - df_24h.price) / df_24h.price
+    df_last["ATH price (€)"] = ATH = df.groupby("symbol").max().price
+    df_last["ATH value (€)"] = df.groupby("symbol").max().value
+    df_last["ATL value (€)"] = df.groupby("symbol").min().value
+    ATL = df.groupby("symbol").min().price
+    df_last["ATH change (%)"] = 100 * (df_last.price - ATH) / ATH
+    df_last["ATL change (%)"] = 100 * (df_last.price - ATL) / ATL
+
+    return df_last
+
+
+def styled_overview_df(df):
+    df_last = overview_df(df)
+    overview = df_last[
+        [
+            "value",
+            "amount",
+            "price",
+            "24h price (%)",
+            "1w price (%)",
+            "ATH change (%)",
+            "ATL change (%)",
+            "ATH price (€)",
+            "ATH value (€)",
+        ]
+    ].sort_values("value", ascending=False)
+
+    def color_negative_red(val):
+        """
+        Takes a scalar and returns a string with
+        the css property `'color: red'` for negative
+        strings, black otherwise.
+        """
+        color = "red" if val < 0 else "green"
+        return "color: %s" % color
+
+    net_worth = df_last.value.sum()
+    overview["value"] = overview["value"].apply(
+        lambda x: f"€{x:.2f} ({100*x/net_worth:.1f}%)"
+    )
+    pct_cols = [c for c in overview.columns if "%" in c]
+    format = {c: "{:+.2f}%" for c in pct_cols}
+    format["ATH value (€)"] = "€{:.2f}"
+    format["amount"] = "{:.4f}"
+    for x in ["price", "ATH price (€)", "ATL price (€)"]:
+        format[x] = "€{:.4f}"
+
+    return (
+        overview.style.applymap(color_negative_red, subset=pd.IndexSlice[:, pct_cols])
+        .format(format, na_rep="-")
+        .bar(subset=["ATL change (%)"], color=["lightgreen"], align="left")
+        .bar(subset=["ATH change (%)"], color=["red"], align="zero")
+    )
