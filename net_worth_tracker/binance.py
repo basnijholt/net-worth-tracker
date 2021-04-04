d13f728a16785e0dd8689e69b0a1451b17065c42
Bas Nijholt <bas@nijho.lt>
2021-04-04 13:17:52 +0200

    add binance.py

---
+++ net_worth_tracker/binance.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+from functools import lru_cache
+
+from binance.client import Client
+
+from .utils import read_config
+
+
+def get_binance_client() -> Client:
+    config = read_config()["binance"]
+    return Client(config["api_key"], config["api_secret"])
+
+
+@lru_cache
+def get_binance_balances():
+    client = get_binance_client()
+    balances = defaultdict(float)
+
+    def normalize(coin):
+        return coin if not coin.startswith("LD") else coin[2:]
+
+    # Get wallet and savings
+    latest_info = max(
+        client.get_account_snapshot(type="SPOT")["snapshotVos"],
+        key=lambda x: x["updateTime"],
+    )["data"]
+    for d in latest_info["balances"]:
+        amount = float(d["free"]) + float(d["locked"])
+        if amount > 0:
+            balances[normalize(d["asset"])] += amount
+
+    # Add Liquidity pool shares
+    uri = "https://api.binance.com/sapi/v1/bswap/liquidity"
+    pools = client._request("get", uri, True, data=dict())
+    assets = [p["share"]["asset"] for p in pools]
+    for d in assets:
+        for coin, amount in d.items():
+            amount = float(amount)
+            if amount > 0:
+                balances[coin] += amount
+
+    return dict(balances)
