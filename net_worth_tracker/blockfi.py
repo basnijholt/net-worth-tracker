77979dcd66acc4bf36d79f01ea90590f3d136235
Bas Nijholt <bas@nijho.lt>
2021-04-04 13:19:58 +0200

    add blockfi.py

---
+++ net_worth_tracker/blockfi.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+
+import pandas as pd
+
+
+@lru_cache
+def get_blockfi_balances(csv_fname: str = "~/Downloads/transactions.csv"):
+    print("Download csv from https://app.blockfi.com/settings/reports")
+    df = pd.read_csv(csv_fname)
+    summed = df.groupby("Cryptocurrency").sum()
+    return {i: row.Amount for i, row in summed.iterrows()}
