645c5c27864d533b33331a217ac137bf5a6bb479
Bas Nijholt <bas@nijho.lt>
2021-04-10 18:50:18 +0200

    add old exodus wallet

--- net_worth_tracker/exodus.py
+++ net_worth_tracker/exodus.py
@@ -2,19 +2,28 @@ import math
 from collections import defaultdict
 from functools import lru_cache
 from pathlib import Path
+from typing import Optional
 
 import pandas as pd
 
 
 @lru_cache
-def get_exodus(csv_fname: str = "~/Desktop/exodus-exports/"):
-    csv = sorted(Path(csv_fname).expanduser().glob("*csv"))[-1]
+def get_exodus(
+    csv_fname: Optional[str] = None,
+    csv_folder: str = "~/Desktop/exodus-exports/",
+):
+    if csv_fname is None:
+        csv_fname = sorted(Path(csv_folder).expanduser().glob("all-txs-*.csv"))[-1]
     balances = defaultdict(float)
-    df = pd.read_csv(csv)
+    df = pd.read_csv(Path(csv_fname).expanduser())
     for i, row in df.iterrows():
         if isinstance(row.INCURRENCY, str) or not math.isnan(row.INCURRENCY):
             balances[row.INCURRENCY] += row.INAMOUNT
-        if isinstance(row.OUTCURRENCY, str) or not math.isnan(row.OUTCURRENCY):
-            balances[row.OUTCURRENCY] += row.OUTAMOUNT + row.FEEAMOUNT
 
-    return {k: dict(amount=v) for k, v in balances.items() if v > 0}
+        if isinstance(row.OUTCURRENCY, str) or not math.isnan(row.OUTCURRENCY):
+            balances[row.OUTCURRENCY] += row.OUTAMOUNT
+
+        if isinstance(row.FEECURRENCY, str) or not math.isnan(row.FEECURRENCY):
+            balances[row.FEECURRENCY] += row.FEEAMOUNT
+
+    return {k: dict(amount=v) for k, v in balances.items() if v > 1e-12}
