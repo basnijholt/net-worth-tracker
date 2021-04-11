874a825456797307258bc79f7754ee52a7ae1050
Bas Nijholt <bas@nijho.lt>
2021-04-11 22:32:53 +0200

    add btcb

--- net_worth_tracker/coin_gecko.py
+++ net_worth_tracker/coin_gecko.py
@@ -14,6 +14,7 @@ def get_coins(balances, cg: CoinGeckoAPI):
         "onx": "OnX Finance",
         "bunny": "Pancake Bunny",
         "rune": "THORChain",
+        "btcb": "Binance Bitcoin",
     }
     symbols = [c.lower() for c in balances]
 
