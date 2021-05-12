b843392b8094b24a2a685fc67d3bd18fffa417b1
Bas Nijholt <bas@nijho.lt>
2021-05-12 11:25:09 +0200

    add stx

--- net_worth_tracker/coin_gecko.py
+++ net_worth_tracker/coin_gecko.py
@@ -14,6 +14,7 @@ def get_coins(balances, cg: CoinGeckoAPI):
         "rune": "THORChain",
         "btcb": "Binance Bitcoin",
         "mash": "MarshmallowDeFi",
+        "stx": "Stacks",
     }
     symbols = [c.lower() for c in balances]
 
