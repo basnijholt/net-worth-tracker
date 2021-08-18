a0e91979323d2c60bf47850b32f65f28f945cfa6
Bas Nijholt <bas@nijho.lt>
2021-08-18 10:50:14 +0200

    add more ignore tokens

--- net_worth_tracker/binance_smart_chain.py
+++ net_worth_tracker/binance_smart_chain.py
@@ -13,7 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from net_worth_tracker.utils import euro_per_dollar, read_config
 
-IGNORE_TOKENS = {"ONX", "Long", "ALPACA"}
+IGNORE_TOKENS = {"ONX", "Long", "ALPACA", "FLUX", "ICA"}
 
 # Pool/vault -> coin name
 LP_MAPPING = {
