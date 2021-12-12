879722c433ac81c773e0f3f7920031753a5902f4
Bas Nijholt <bas@nijho.lt>
2021-12-12 21:33:16 +0000

    use darkmode

--- run-and-upload.sh
+++ run-and-upload.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
 . ~/.bash_profile
-jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute crypto-tracker.ipynb  --no-input
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute crypto-tracker.ipynb --no-input --HTMLExporter.theme=dark
 fu crypto-tracker.html
 fu current_value
