e05737527c417af165e4c53c033781fe4546e7c0
Bas Nijholt <bas@nijho.lt>
2021-12-12 15:57:22 +0100

    add plot_barh_at_date_with_profits

--- net_worth_tracker/plots.py
+++ net_worth_tracker/plots.py
@@ -94,3 +94,50 @@ def plot_barh_at_date(df, date, min_euro=10, show=True, fname=None, fig=None, ax
         plt.savefig(fname)
     if show:
         plt.show()
+
+
+def plot_barh_at_date_with_profits(
+    df, date, min_euro=10, show=True, fname=None, fig=None, ax=None
+):
+    if fig is None:
+        fig, ax = plt.subplots(figsize=(15, 15))
+    else:
+        ax.clear()
+    to_drop = df.value < min_euro
+
+    sel = (
+        df[(~to_drop) & (df.date == date)]
+        .set_index("symbol")
+        .sort_values(ascending=True, by="value")
+    )
+    value = sel.value
+    profits = sel.amount * (sel.price - sel.avg_price)
+    colors = ["r" if p < 0 else "g" for p in profits]
+
+    bars = ax.barh(value.index, value, color="grey")
+    _ = ax.barh(profits.index, profits.abs(), color=colors, alpha=0.2, hatch="//")
+
+    factor = 100 / value.sum()
+    labels = [
+        f"{coin} - €{amount:.2f} ({factor*amount:1.2f}%)"
+        for coin, amount in value.items()
+    ]
+
+    for label, bar in zip(labels, bars):
+        width = bar.get_width()
+        ax.text(
+            width * 1.01,
+            bar.get_y() + bar.get_height() / 2,
+            f"{label}",
+            ha="left",
+            va="center",
+        )
+
+    ax.set_title(f"Value at {date}")
+    ax.set_axis_off()
+    ax.set_xlim(0, 1.2 * df.value.max())
+
+    if fname is not None:
+        plt.savefig(fname)
+    if show:
+        plt.show()
